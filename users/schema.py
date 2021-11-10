dcc250119fd2b66f53c97c13891948aba377e2b0
Ivan <toivan777@yahoo.com>
2021-11-10 21:50:13 +0300

    1

---
+++ users/schema.py
@@ -0,0 +1,16 @@
+import graphene
+from graphql_auth import mutations
+from graphql_auth.schema import UserQuery, MeQuery
+
+class AuthMutation(graphene.ObjectType):
+   register = mutations.Register.Field()
+   verify_account = mutations.VerifyAccount.Field()
+
+class Query(UserQuery, MeQuery, graphene.ObjectType):
+    pass
+
+class Mutation(AuthMutation, graphene.ObjectType):
+   pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
